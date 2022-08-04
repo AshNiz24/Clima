fbe275e8cb7d3a70aed78119f20a8af65be397a8
Ashna Nizam <68196153+AshNiz24@users.noreply.github.com>
2022-08-04 19:29:07 +0530

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,2 +1,2 @@
 const String domain = 'https://api.openweathermap.org/data/2.5/weather?';
-const String apiKey = '7ac2eff9e26dc73f98c8cd4489fb972b';
+const String apiKey = 'PASTE YOUR API KEY HERE';
